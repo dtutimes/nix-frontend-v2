ec90493a9db4f0b2fb78fd5960dd109f111d22d4
Shobhit Tiwari <122107079+Shobhit141141@users.noreply.github.com>
2024-02-25 19:11:59 +0530

    sidebar-page conflict fix

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -4,7 +4,7 @@ import SideNavigation from "@/components/SideNavigation/sideNavigation";
 const Layout = ({ children }) => {
   return (
     <div className="flex">
-      <div className="mr-[50px]"><SideNavigation /></div>
+      <div className="mr-[60px]"><SideNavigation /></div>
     
       <main className="flex-grow bg-[var(--bg-color)] p-4">
         {children}
